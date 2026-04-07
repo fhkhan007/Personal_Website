ec93d14cddd5bef13018b69bc664c10fdd7aa0f8
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2026-04-07 16:43:32 +0600

    Merge branch 'main' of https://github.com/fhkhan007/Personal_Website into development

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -14,11 +14,11 @@ const person: Person = {
   firstName: "Md. Fahad",
   lastName: "Hossain",
   name: `Md. Fahad Hossain`,
-  role: "Design Engineer",
+  role: "Frontend Developer",
   avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  email: "fahadhossainkhan@gmail.com",
+  location: "Asia/Dhaka", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Bangla"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
@@ -34,13 +34,13 @@ const social: Social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/once-ui-system",
+    link: "",
     essential: true,
   },
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    link: "https://github.com/fhkhan007",
     essential: true,
   },
   {
